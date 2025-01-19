c6d76a5fe66f86b8357b906adb9842be1e1deef8
Stanislav Kolobaiev <115995792+skolobaev13@users.noreply.github.com>
2025-01-19 23:55:50 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,6 @@
-const sortByDescendingFriendCount = (users) =>
-  [...users].sort((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = (users) => {
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+};
 
 console.log(
   sortByDescendingFriendCount([
